c703119ae9ac886a49bbbdc223522e33c6e2cd0a
EduardoCastelan <eduardogutierrez521@gmail.com>
2018-11-01 18:09:31 -0700

    graph still not being updated

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var socket = require('socket.io');  //Socket I/O for seemless multiusers (librar
 const fs = require('fs');           //To read file (library)
 var simulating = false;
 let obj = [];
-let id = randomNumber(1000, 1500); //starting id value (start year) based on following random number
-
-
+//let id = randomNumber(1000, 1500); //starting id value (start year) based on following random number
+var getNewJSON = require('./getNewJSON'); // getting module
+let id ;// =  randomNumber(); //randomNumber(1000, 1500); //starting id value (start year) based on following random number <from Robert> 
 console.log("test")
 
 // App setup
@@ -25,8 +25,7 @@ var io = socket(server);
 
 io.on('connection',function(socket){ // Fires callback function when client connects
 	if(simulating){
-        console.log('Simulating to new client...');
-        
+        console.log('Simulating to new client...');  
 	}
 
 	else{
@@ -41,22 +40,33 @@ io.on('connection',function(socket){ // Fires callback function when client conn
 //Sends data to clients [every 5 seconds]
 function generate(){
 
-    id++; //increment for next data entry (monster attack)
+    //id++; //increment for next data entry (monster attack)    <from Robert> 
+    randomNumber();
+    setTimeout(function() {
+        getNewJSON.start(), 3500;
+    })
 
 	io.sockets.emit('simulated-data', obj[id]); 
     console.log("Sending again...")
-    console.log(obj[id]); //Test purposes
+    console.log("printing ", obj[id]); //Test purposes
 	setTimeout(generate, 5000);
 }
 
 
 //Reads the file and parses everything into an object
 function readMe(){
-    obj = JSON.parse(fs.readFileSync('public/quakes.json', 'utf8'));
+    //obj = JSON.parse(fs.readFileSync('public/quakes.json', 'utf8'));
+    obj = JSON.parse(fs.readFileSync('public/combined.json', 'utf8')); //read file from combines.json
     setTimeout(generate, 2000)
 }
 
 //Randomly selects start year
 function randomNumber(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    id  = Math.floor(Math.random() * obj.length) ; 
+    
+    if (id == 0)
+        randomNumber();
+    else
+        id = id;
+    //return Math.floor(Math.random() * (max - min + 1) + min);
 }
--- public/simulate.js
+++ public/simulate.js
@@ -1,53 +1,71 @@
 //Create a connection
-var socket = io.connect('http://localhost:4001');
+var socket = io.connect("http://localhost:4001");
 
+socket.on("simulated-data", function(data) {
+  //console.log(data); //print whole data object (test purposes)
 
-socket.on('simulated-data', function (data){
+  var id = data.A;
+  var flag_tsunami = data.B;
+  var year = data.C;
+  var month = data.D;
+  var day = data.E;
+  var hour = data.F;
+  var minute = data.G;
+  var second = data.H;
+  var magnitude = data.I;
+  var intensity = data.J;
+  var country = data.K;
+  var location_name = data.L;
+  var latitude = data.M;
+  var longitude = data.N;
+  var deaths = data.O;
+  var injuries = data.Q;
 
-    //console.log(data); //print whole data object (test purposes) 
+  var displaydata =
+    id +
+    " " +
+    flag_tsunami +
+    " " +
+    year +
+    " " +
+    month +
+    " " +
+    day +
+    " " +
+    hour +
+    " " +
+    minute +
+    " " +
+    second +
+    " " +
+    country +
+    " " +
+    location_name; //Test purposes
 
-    var id = data.A;
-    var flag_tsunami = data.B;
-    var year = data.C;
-    var month = data.D;
-    var day = data.E;
-    var hour = data.F;
-    var minute = data.G;
-    var second = data.H;
-    var magnitude = data.I;
-    var intensity = data.J;
-    var country = data.K;
-    var location_name = data.L;
-    var latitude = data.M;
-    var longitude = data.N;
-    var deaths = data.O;
-    var injuries = data.Q;
-    
-    console.log(id + " " + flag_tsunami + " " + year + " " + month + " " + day + " " + hour + " " + minute + " " + second + " " + country + " " + location_name); //Test purposes
-    
-    
-
-    output.innerHTML = data.A ; //Display data onto html page
+  output.innerHTML = displaydata; //Display data onto html page
+  
+  document.write(
+    '<px-map-marker-static id="Godzilla" style="display:none;" lat="-44.245866" lng="-13.772970" type="info"> <px-map-popup-data title="Godzilla" data=\'{"Kills":"2000","damage":"etc","magnitude":"8.0"}\'> </px-map-popup-data> </px-map-marker-static>'
+  );
 });
 
-
-    // obj[0].A - "ID"
-    // obj[0].B - "FLAG_TSUNAMI"
-    // obj[0].C - "YEAR"
-    // obj[0].D - "MONTH"
-    // obj[0].E - "DAY"
-    // obj[0].F - "HOUR"
-    // obj[0].G - "MINUTE"
-    // obj[0].H - "SECOND"
-    // obj[0].I - "EQ_MAG_ML"
-    // obj[0].J - "INTENSIT"
-    // obj[0].K - "COUNTRY"
-    // obj[0].L - "LOCATION_NAME"
-    // obj[0].M - "LATITUDE"
-    // obj[0].N - "LONGITUDE"
-    // obj[0].O - "DEATHS"
-    // obj[0].P - "MISSING"
-    // obj[0].Q - "INJURIES"
-    // obj[0].R - "DAMAGE_MILLIONNS_DOLLARS"
-    // obj[0].S - "HOUSES_DESTROYED
-    // obj[0].T - "HOUSES_DAMAGED"
+// obj[0].A - "ID"
+// obj[0].B - "FLAG_TSUNAMI"
+// obj[0].C - "YEAR"
+// obj[0].D - "MONTH"
+// obj[0].E - "DAY"
+// obj[0].F - "HOUR"
+// obj[0].G - "MINUTE"
+// obj[0].H - "SECOND"
+// obj[0].I - "EQ_MAG_ML"
+// obj[0].J - "INTENSIT"
+// obj[0].K - "COUNTRY"
+// obj[0].L - "LOCATION_NAME"
+// obj[0].M - "LATITUDE"
+// obj[0].N - "LONGITUDE"
+// obj[0].O - "DEATHS"
+// obj[0].P - "MISSING"
+// obj[0].Q - "INJURIES"
+// obj[0].R - "DAMAGE_MILLIONNS_DOLLARS"
+// obj[0].S - "HOUSES_DESTROYED
+// obj[0].T - "HOUSES_DAMAGED"
